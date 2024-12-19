57c4391fae2df78b299f71106a93a1cd9e10671a
Maruf-PC <Maruf-PC@DESKTOP-DIG86MK>
2024-12-19 23:27:29 +0600

    CRUD with Interfaces

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,7 +11,11 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddScoped<IProductRepository, ProductRepository>();//a choice of the lifetime of how long is this service going to be available for. 
+                                                                    //Scoped: the service is going to live for as long as the HTTP request. It's scoped to the lifetime of the HTTP request.
+                                                                    //Transient: this is effectively scoped to the method level, not the request level.
+                                                                    //Singleton: this is going to create the service when the application starts up and will not dispose of the service until the application shuts down.
+                                                                    // Lea
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
