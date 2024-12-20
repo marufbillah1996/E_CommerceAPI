b767ac32f3b5cde03d68a88cf74407c704fdf6f4
Maruf-PC <Maruf-PC@DESKTOP-DIG86MK>
2024-12-20 12:21:21 +0600

    filter as query params

---
+++ Core/Specifications/ProductSpecParams.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    public class ProductSpecParams
+    {
+        private List<string> _brands = [];
+        public List<string> Brands
+        {
+            get => _brands;
+            set {
+            _brands = value.SelectMany(x=>x.Split(',',StringSplitOptions.RemoveEmptyEntries)).ToList();
+            }
+        }
+        private List<string> _types = [];
+        public List<string> Types
+        {
+            get => _types;
+            set
+            {
+                _types = value.SelectMany(x => x.Split(',', StringSplitOptions.RemoveEmptyEntries)).ToList();
+            }
+        }
+        public string? Sort { get; set; }
+    }
+}
