f85a1752baf723a2b276b2d04d9e3850448556a6
Maruf-PC <Maruf-PC@DESKTOP-DIG86MK>
2024-12-20 14:17:31 +0600

    Searching implementation end of section 5

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -34,8 +34,18 @@ namespace Core.Specifications
         public int PageSize
         {
             get => _pageSize;
-            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;               
         }
+
+
+        private string? _search;
+
+        public string Search
+        {
+            get => _search ?? "";
+            set => _search = value.ToLower();
+        }
+
 
 
     }
