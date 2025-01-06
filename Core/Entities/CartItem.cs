57ec3a93e3505a6d2e2705cdbf9dbc95dfdfd6ba
Maruf-PC <Maruf-PC@DESKTOP-DIG86MK>
2025-01-06 16:06:03 +0600

    cart controller and End of section 12

--- Core/Entities/CartItem.cs
+++ Core/Entities/CartItem.cs
@@ -10,11 +10,12 @@ namespace Core.Entities
     {
         public int ProductId { get; set; }
         public required string ProductName { get; set; }
-        public required string Description { get; set; }
+        //public required string Description { get; set; }
         public decimal Price { get; set; }
-        public int Quantity { get; set; }
         public required string PictureUrl { get; set; }
         public required string Type { get; set; }
         public required string Brand { get; set; }
+        public int Quantity { get; set; }
+
     }
 }
