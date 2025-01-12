729a8c8a991c7e0451c924314b005e195c9e6d24
Maruf-PC <Maruf-PC@DESKTOP-DIG86MK>
2025-01-12 11:39:56 +0600

    loging,logout,authstatue,userinfo working

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,10 @@
 ﻿using API.DTOs;
 using Core.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 
 namespace API.Controllers
 {
@@ -20,6 +23,32 @@ namespace API.Controllers
             var result = await signInManager.UserManager.CreateAsync(user,registerDTO.Password);
             if(!result.Succeeded) return BadRequest(result.Errors);
             return Ok();
+        }
+        [Authorize]
+        [HttpPost("logout")]
+        public async Task<ActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return NoContent();
+        }
+        [HttpGet("user-info")]
+        public async Task<ActionResult> GetUserInfo()
+        {
+            if(User.Identity?.IsAuthenticated == false) return NoContent();
+
+            var user = await signInManager.UserManager.Users
+                .FirstOrDefaultAsync(x=>x.Email == User.FindFirstValue(ClaimTypes.Email));
+            if(user == null) return Unauthorized();
+
+            return Ok(new
+            {
+                user.FirstName, user.LastName,user.Email
+            });
+        }
+        [HttpGet]
+        public ActionResult GetAuthState()
+        {
+            return Ok(new {IsAuthenticated = User.Identity?.IsAuthenticated ?? false});
         }
 
     }
